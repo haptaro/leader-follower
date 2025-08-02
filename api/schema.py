398206c342211a6719d05c64631e8661c1796496
Kotaro Fukuo <kotaro56f@gmail.com>
2025-08-02 09:01:44 -0700

    Added Mutation

--- api/schema.py
+++ api/schema.py
@@ -1,8 +1,9 @@
 import strawberry
 import strawberry_django
 from django.contrib.auth.models import User
+from api.models import Note
 
-from api.types import UserType
+from api.types import UserType, AddNoteInput, NoteType
 
 @strawberry.type
 class Query:
@@ -14,5 +15,15 @@ class Query:
         else:
             return None
 
+@strawberry.type
+class Mutation:
+    @strawberry_django.mutation
+    def add_note(self, info, input: AddNoteInput) -> NoteType:
+        user = info.context.request.user
+        if not user.is_authenticated:
+            raise Exception("Authentication required")
+        
+        note = Note.objects.create(message=input.message, user=user)
+        return note
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(query=Query, mutation=Mutation)
--- api/types.py
+++ api/types.py
@@ -1,9 +1,21 @@
 import strawberry
 import strawberry_django
 from django.contrib.auth.models import User
+from api.models import Note
 
 @strawberry_django.type(User)
 class UserType:
     id: strawberry.auto
     username: strawberry.auto
     email: strawberry.auto
+
+@strawberry_django.type(Note)
+class NoteType:
+    id: strawberry.auto
+    message: strawberry.auto
+    created_at: strawberry.auto
+    user: UserType
+
+@strawberry_django.input(Note)
+class AddNoteInput:
+    message: strawberry.auto
