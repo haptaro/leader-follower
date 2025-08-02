96a093ad419fb0e2d9205a42bfe265bb966b1de2
Kotaro Fukuo <kotaro56f@gmail.com>
2025-08-02 08:46:16 -0700

    Created a new field

--- api/models.py
+++ api/models.py
@@ -3,6 +3,11 @@ from django.db import models
 class Note(models.Model):
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        'auth.User',
+        related_name='notes',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.message[:30]
