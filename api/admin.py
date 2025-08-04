616e8088238a90c09a43573d85c76b648cb5cf89
Kotaro Fukuo <kotaro56f@gmail.com>
2025-08-03 22:52:33 -0700

    Added admin

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, UserMetadata, ChatRoom, ChatRoomMember, Message
 
 admin.site.register(Note)
+admin.site.register(UserMetadata)
+admin.site.register(ChatRoom)
+admin.site.register(ChatRoomMember)
+admin.site.register(Message)
